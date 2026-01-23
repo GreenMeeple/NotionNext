c042f2e634ccb7676ad33b3ca10aa19ad4fac67f
Li Chun Ngai, Alex <alexcnli@yahoo.com>
2026-01-23 18:32:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,9 +4,9 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '278aa3ab4b7e8079a619d04d7ab9d90c,en:2f1aa3ab4b7e81579738000cd761e965',
+    '278aa3ab4b7e8079a619d04d7ab9d90c,en:2f1aa3ab4b7e802a9e0ee020948f207c',
   THEME: process.env.NEXT_PUBLIC_THEME || 'simple', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
-  LANG: process.env.NEXT_PUBLIC_LANG || 'en-US', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
+  LANG: process.env.NEXT_PUBLIC_LANG || 'zh-HK', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
 
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
